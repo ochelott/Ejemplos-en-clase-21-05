b42e01e4601673473f73e2149e3cb294fda9010c
Holotte Sebastian Benjamin <benjash8@outlook.com>
2026-05-21 16:16:55 -0300

    Ejemplo 2

---
+++ Ejemplo 2/Ejemplo 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0; int acum = 0; int cont = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Titulo 1");
+                Console.WriteLine("1 _ Ingresar Nro");
+                Console.WriteLine("2 _ Calcular y mostrar promedio");
+                Console.WriteLine("Otro - Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Ingresar el nro");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        acum += num;
+                        cont++;
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Titulo pantalla 2");
+                        double promedio = 0;
+                        if (cont > 0)
+                        {
+                            promedio = 1.0 * acum / cont;
+                            Console.WriteLine($"Promedio: {promedio:F2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No hubo ingresos");
+                        }
+                        Console.WriteLine("Presione una tecla para volver");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        opcion = -1;
+                        break;
+                }
+
+
+            } while (opcion == -1);
+            Console.WriteLine("Presionar una tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
