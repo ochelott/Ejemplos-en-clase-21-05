3de44c6dda9f3ade723b9b1b2219167fcf0baabd
Holotte Sebastian Benjamin <benjash8@outlook.com>
2026-05-21 17:53:10 -0300

    Ejemplo 3

---
+++ Ejemplo 3/Ejemplo 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
